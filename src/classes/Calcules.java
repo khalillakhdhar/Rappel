74b063d837dcef6445a63c520c7078efae3e8dbe
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-15 11:34:53 +0100

    calcule

--- src/classes/Calcules.java
+++ src/classes/Calcules.java
@@ -1,5 +1,27 @@
 package classes;
 
 public class Calcules {
+	private String dateheure;
+	private String etat;
+	public String getDateheure() {
+		return dateheure;
+	}
+	public void setDateheure(String dateheure) {
+		this.dateheure = dateheure;
+	}
+	public String getEtat() {
+		return etat;
+	}
+	public void setEtat(String etat) {
+		this.etat = etat;
+	}
+	public Calcules() {
+		super();
+	}
+	public int indice(Personne p)
+	{
+	return (int) Math.round(p.getPoid()/Math.pow(p.getTaille(),2));
+		
+	}
 
 }
--- src/classes/Personne.java
+++ src/classes/Personne.java
@@ -1,5 +1,41 @@
 package classes;
 
 public class Personne {
+private String nom,prenom;
+private float poid,taille;
+public String getNom() {
+	return nom;
+}
+public void setNom(String nom) {
+	this.nom = nom;
+}
+public String getPrenom() {
+	return prenom;
+}
+public void setPrenom(String prenom) {
+	this.prenom = prenom;
+}
+public float getPoid() {
+	return poid;
+}
+public void setPoid(float poid) {
+	this.poid = poid;
+}
+public float getTaille() {
+	return taille;
+}
+public void setTaille(float taille) {
+	this.taille = taille;
+}
+public Personne(String nom, String prenom, float poid, float taille) {
+	super();
+	this.nom = nom;
+	this.prenom = prenom;
+	this.poid = poid;
+	this.taille = taille;
+}
+public Personne() {
+	super();
+}
 
 }
